3039022e453a32c921793264297cd136409af8e7
Ernesto Pena <ernestopenad@gmail.com>
2019-07-21 19:26:26 -0500

    functions inside Action were created. Exporting both actions with the type and payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
-class Counter extends Component {
+class Counter extends React.Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
@@ -14,13 +14,10 @@ class Counter extends Component {
     };
 
     render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { const newCount = props.count + 1}}>
                     +
                 </button>
                 <button onClick={() => {/* Fill me in */ }}>
@@ -39,21 +36,12 @@ class Counter extends Component {
     }
 }
 
-// The mapStateToProps function specifies which portion of the
-// state tree this component needs to receive. In this case,
-// since our redux store is only storing the value of the count,
-// this component receives the whole state. In a more complex
-// redux application, though, it would receive only the relevant
-// parts it needs from the state object.
+
 const mapStateToProps = (state) => {
     return {
         count: state.count
     };
 };
 
-// The connect function is called in order to make this component aware
-// of the rest of the redux architecture. Without this, this component
-// is only a dumb React component. We pass in all of the functions that
-// are reliant on Redux, along with the component itself, so that Redux
-// makes itself known to this component.
+
 export default connect(mapStateToProps, { increment, decrement })(Counter);
